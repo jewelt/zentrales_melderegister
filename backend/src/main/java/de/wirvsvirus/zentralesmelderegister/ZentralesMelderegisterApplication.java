9c3c4897cc108169eb214442a5ed03c1d36e1483
Henning Wobken <henning.wobken@simplex24.de>
2020-03-20 23:16:37 +0100

    init

---
+++ backend/src/main/java/de/wirvsvirus/zentralesmelderegister/ZentralesMelderegisterApplication.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.zentralesmelderegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentralesMelderegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentralesMelderegisterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/wirvsvirus/zentralesmelderegister/ZentralesMelderegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.zentralesmelderegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentralesMelderegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
