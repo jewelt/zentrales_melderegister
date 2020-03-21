dbde72921da1e9eadf153853b275b4561e99c564
paddy <jon-patric.ewelt@simplex24.de>
2020-03-21 16:53:06 +0100

    test controller mit patienten und ergebnisses drin

---
+++ backend/src/main/java/de/wirvsvirus/zentralesmelderegister/model/TestPatientTestResultDTO.java
@@ -0,0 +1,26 @@
+package de.wirvsvirus.zentralesmelderegister.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.OffsetDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@Data
+public class TestPatientTestResultDTO extends TestDTO {
+
+    private PatientDTO patientDTO;
+    private TestResultDTO testResultDTO;
+
+    public TestPatientTestResultDTO(TestDTO testDTO) {
+        super.setId(testDTO.getId());
+        this.setEntryDate(testDTO.getEntryDate());
+        this.setTestDate(testDTO.getTestDate());
+        this.setResultDate(testDTO.getResultDate());
+        this.setTestResultId(testDTO.getTestResultId());
+        this.setPatientId(testDTO.getPatientId());
+    }
+
+}
